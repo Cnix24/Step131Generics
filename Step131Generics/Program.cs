a22c2bbcca00240808e0b7b047b27e97ee18440a
Cnix24 <calvinattl24@gmail.com>
2019-07-22 15:11:29 -0600

    Update

---
+++ Step131Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step131Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string> { Things = new List<string> { "Name1", "Name2" } };
+            Employee<int> employeeNum = new Employee<int> { Things = new List<int> { 4, 8 } };
+
+            employee.Things.ForEach(Console.WriteLine);
+            employeeNum.Things.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+    }
+}
